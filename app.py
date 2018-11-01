fdbb5634cb51dbc25aec17770e5d1dd1338ddead
root <root@vm-0ae4ca06.netangels.ru>
2018-11-01 19:04:23 +0000

    writes new tasks to a file

--- app.py
+++ app.py
@@ -1,18 +1,69 @@
 from flask import Flask, redirect, url_for, request
 from flask import render_template
+
+
+
 #, flash, redirect, render_template, request, session, abort
 app = Flask(__name__)
 
 
 @app.route('/')
-def hell(name=None):
+def hell():
+
+#    global res
+#    global res1
+#    global res2
+#    global res3
+
+
 #    return "<h1>Hello World!</h1>"
 #    return render_template('hello.html', name="new user")
 #    return "List of task"
      with open("test.txt","r") as fo:
-        res = fo.read()
+        if len(fo.read().split('\n')) == 0:
+            res = res1 = res2 = res3 = " "
+
+        if len(fo.read().split('\n')) == 1:
+            res = res1 = res2 = res3 = " "
+#            return render_template('tasks.html', name = res, name1 = res1, name2 = res2, name3 = res3)
+#            fo.seek(0)
+        fo.seek(0)
+        if len(fo.read().split('\n')) == 2:
+            res = fo.read().split('\n')[0]
+            fo.seek(0)
+            res1 = fo.read().split('\n')[1]
+            res2 = res3 = " "
+#            return render_template('tasks.html', name = res, name1 = res1, name2 = res2, name3 = res3)
+
+        fo.seek(0)
+        if len(fo.read().split('\n')) == 3:
+            res = fo.read().split('\n')[0]
+            fo.seek(0)
+            res1 = fo.read().split('\n')[1]
+            fo.seek(0)
+            res2 = fo.read().split('\n')[2]
+            fo.seek(0)
+            res3 = " "
+#            return render_template('tasks.html', name = res, name1 = res1, name2 = res2, name3 = res3)
+
+        fo.seek(0)
+        if len(fo.read().split('\n')) == 4:
+            res = fo.read().split('\n')[0]
+            fo.seek(0)
+            res1 = fo.read().split('\n')[1]
+            fo.seek(0)
+            res2 = fo.read().split('\n')[2]
+            fo.seek(0)
+            res3 = fo.read().split('\n')[3]
+            fo.seek(0)
+        return render_template('tasks.html', name = res, name1 = res1, name2 = res2, name3 = res3)
+#        else:
+#            return "fuck"
+#        res4 = fo.read().split(' ')[4]
+
 #         fo.flush()
-     return "Here tasks go: " + res
+#     return "Here tasks go: " + res
+#     return render_template('tasks.html', name = res, name1 = res1, name2 = res2, name3 = res3)
 
 
 
@@ -43,6 +94,7 @@ def login():
    if request.method == 'POST': 
       user = request.form['nm'] 
       with open("test.txt","a") as fo:
+         fo.write("\n")
          fo.write(user)
          fo.flush()
       return redirect(url_for('success',name = user)) 
